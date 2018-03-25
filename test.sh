faef9f3e04b7d0bc9f9f4ea8bc8e3a6e63fa30e3
sihc <tvquocchi@gmail.com>
2018-03-25 12:04:22 +0700

    third submit

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 git add .
 git status
+echo "commit -m $1"
 git commit -m "$1"
 if [ $# = 2 ]
 then
+	echo "git push -u origin $2"
 	git push -u origin $2
 else
+	echo "git push -u origin master"
 	git push -u origin master
 fi
 
